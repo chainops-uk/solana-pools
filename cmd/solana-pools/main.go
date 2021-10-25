1ec7c3975b2f594d60ef1099af6cbf2e506bb7da
Vadym Mozharivsky <vdm@everstake.one>
2021-10-25 10:12:49 +0300

    update gitignore

---
+++ cmd/solana-pools/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dfuse-io/solana-go/rpc"
+	"github.com/everstake/solana-pools/internal/services/pools/parrot"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func main()  {
+	var command = &cobra.Command{
+		Use:   "solana pools",
+		Short: "start solana pools application",
+		Long:  `start solana pools application`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			rpcCli := rpc.NewClient("https://api.mainnet-beta.solana.com")
+			pool := parrot.New(rpcCli)
+			poolData, err  := pool.GetData("AMjGNE12gNoZnrU68AGxUibYEjrGPgpPk3EYG5MZCiZQ")
+			if err != nil {
+				return fmt.Errorf("pool.GetData: %s", err.Error())
+			}
+			_ = poolData
+			return nil
+		},
+	}
+
+	if err := command.Execute(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
