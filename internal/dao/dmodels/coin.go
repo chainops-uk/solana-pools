58125e180514ce5e6b8f64abf8f136307646986d
Andrey Pugach <apu@everstake.one>
2021-12-20 14:37:07 +0200

    latest

--- internal/dao/dmodels/coin.go
+++ internal/dao/dmodels/coin.go
@@ -6,7 +6,7 @@ type Coin struct {
 	ID         uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4();not null;"`
 	Name       string    `gorm:"type:varchar(40);not null;index:idx_coin_name,unique;"`
 	GeckoKey   string    `gorm:"type:varchar(40);not null;index:idx_coin_gecko_key,unique;"`
-	Address    string    `gorm:"type:varchar(120);not null;index:idx_coin_address,unique;"`
+	Address    string    `gorm:"type:varchar(120);not null;"` //index:idx_coin_address,unique;
 	USD        float64   `gorm:"type:float8;not null;default:0;"`
 	ThumbImage string    `gorm:"type:varchar(240);not null;default:'NaN';"`
 	SmallImage string    `gorm:"type:varchar(240);not null;default:0;default:'NaN';"`
